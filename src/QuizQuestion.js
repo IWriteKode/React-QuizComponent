1d24595c0766c20bb5fb8a5985557915ae783ec2
Junaid <junaidrehman@devnatives.com>
2022-01-17 18:30:45 +0500

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import QuizQuestionButton from './QuizQuestionButton'
+
 class QuizQuestion extends React.Component {
 
     constructor(props){
@@ -16,7 +18,7 @@ class QuizQuestion extends React.Component {
         <section className="buttons">
           <ul>
              
-            <li>{this.props.quiz_question.answer_options[0]}</li>
+          <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
           </ul>
         </section>
       </main>
---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class QuizQuestionButton  extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            quiz_position : 1
+        }
+    }
+    render() {
+      return (
+       
+          
+             
+            <li>
+                <button>{this.props.button_text}</button>
+            </li>
+           
+        
+      )
+    }
+  }
+export default QuizQuestionButton 
