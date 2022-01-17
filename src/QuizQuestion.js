f97a3267e659572772373b6887575988ce8fed78
Junaid <junaidrehman@devnatives.com>
2022-01-17 18:15:41 +0500

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -11,11 +11,11 @@ class QuizQuestion extends React.Component {
       return (
         <main>
         <section>
-          <p>{this.props.instruction_text}</p>
+          <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
-            //quiz question button logic goes here
+             
             <li>{this.props.quiz_question.answer_options[0]}</li>
           </ul>
         </section>
